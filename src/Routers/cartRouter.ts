8c52044a9ce60553e356226c48a5192ffa3861aa
digvijaybind <52271076+digvijaybind@users.noreply.github.com>
2021-07-14 01:46:12 +0530

    Add files via upload

---
+++ src/Routers/cartRouter.ts
@@ -0,0 +1,34 @@
+import {Router} from 'express'
+import { GlobalMiddleWare } from '../GlobalMiddleWare/GlobalMiddleWare';
+import { categoryController } from '../Controllers/categoryController';
+import { cartController } from '../Controllers/cartController';
+import Product from '../Models/Product';
+import { cartValidators } from '../Validators/cartValidators';
+export class cartRouter{
+     router:Router;
+    constructor(){
+        this.router=Router();
+        this.getRouter();
+        this.postRouter();
+        this.deleteRouter();
+        this.putRouter();
+    }
+
+    getRouter(){
+    //  this.router.get('/',GlobalMiddleWare.authenticate,cartController.get)
+      
+    }
+    postRouter(){
+     
+        this.router.post('/addtocart',GlobalMiddleWare.authenticate,cartValidators.addtocart(),GlobalMiddleWare.checkError,cartController.addtocart)
+      
+}
+    deleteRouter(){
+    
+    }
+    putRouter(){
+ 
+    }
+}
+
+export default new cartRouter().router
